9892820fd30db22e5130af7093d61d7035a10c43
Antonbinom <protest_the_hero@mail.ru>
2021-08-03 15:21:21 +0300

    fix: change bam names

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 	// ---------- Burger Menu -----------
 
-  var menuButton = document.querySelector(".burger-menu__button");
+  var menuButton = document.querySelector(".burger-button");
   menuButton.addEventListener("click", function () {
     document.querySelector(".header__burger-menu").classList.toggle("header__burger-menu--visible");
 		console.log('click');
