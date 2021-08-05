7307c4a9b3e65a94ae2be431bf77b6d2e68b112c
Antonbinom <protest_the_hero@mail.ru>
2021-08-05 19:11:25 +0300

    create: sections mobile

--- js/main.js
+++ js/main.js
@@ -13,34 +13,39 @@ $(document).ready(function(){
 //   // ---------- Hotel Slider -----------
 
   const sectionsSlider = new Swiper('.sections__slider', {
-  // Optional parameters
+  // Default parameters
   slidesPerView: 4,
   spaceBetween: 26,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  // Responsive breakpoints
-  // breakpoints: {
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  // // Responsive breakpoints
+  breakpoints: {
     // when window width is >= 320px
-  //   1440: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 20
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   },
-  //   // when window width is >= 640px
-  //   640: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40
-  //   }
-  // };
-  loop: false,
-  watchOverflow: true,
-  autoHaight: true,
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      slidesPerColumn: 2,
+      slidesPerColumnFill: 'row',
+    },
+    // when window width is >= 640px
+    567: {
+      slidesPerView: 2,
+      spaceBetween: 26,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 26,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 26,
+    },
+  },
+  // loop: false,
+  // watchOverflow: true,
+  // autoHaight: false,
   // Navigation arrows
   navigation: {
     nextEl: '.sections__slider-button--next',
