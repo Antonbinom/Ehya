f70e199346e4c8e5bd9795ed12067d7dc099bd6b
Antonbinom <protest_the_hero@mail.ru>
2021-08-06 09:33:04 +0300

    fix: add class 'hold' for body, fix main

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   var menuButton = document.querySelector(".burger-button");
   menuButton.addEventListener("click", function () {
     document.querySelector(".header__burger-menu").classList.toggle("header__burger-menu--visible");
-		console.log('click');
+    $('body').toggleClass('hold');
   });
 
 //   // ---------- Hotel Slider -----------
